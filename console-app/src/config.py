84deff28b370a7d6048c209977834dd0130f85d5
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2025-02-25 10:35:14 +0100

    planning the architecture for using tkinter

--- console-app/src/config.py
+++ console-app/src/config.py
@@ -4,6 +4,3 @@ is_running = True
 gyroscope = False
 
 controller = DualSenseController(device_index_or_device_info=0, microphone_initially_muted=False, microphone_invert_led=False)
-
-
-import asyncio
